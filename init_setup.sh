1b5751fdee75b9da01e2620c91f4e56ec2cf4ee1
Khan <amirkhan60972@gmail.com>
2022-09-21 00:36:19 +0530

    updated constants

--- init_setup.sh
+++ init_setup.sh
@@ -5,6 +5,4 @@ echo [$(date)] : "Activating the environment"
 source activate ./env
 echo [$(date)] : "installing the dev requirements"
 pip install -r requirements_dev.txt
-echo [$(date)] : "installing dvc"
-pip3 install dvc --user
 echo [$(date)] : "END"
